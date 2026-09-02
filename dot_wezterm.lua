6783c0c749b0eb97356e0317e925ee331c3da7f5
motoish <yuan.zhang@gmx.com>
2026-09-02 09:21:26 +0900

    fix: update wezterm config

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -12,7 +12,8 @@ config.initial_cols = 130
 config.initial_rows = 38
 
 -- 主题
-config.color_scheme = 'Adventure Time (Gogh)'
+-- config.color_scheme = 'Adventure Time (Gogh)'
+config.color_scheme = "Gruvbox dark, hard (base16)"
 -- config.colors = {
 --   tab_bar = {
 --     -- 整条 Tab Bar 背景
@@ -43,10 +44,10 @@ config.color_scheme = 'Adventure Time (Gogh)'
 config.window_decorations = "RESIZE"
 
 config.window_padding = {
-  left = 12,
-  right = 12,
-  top = 10,
-  bottom = 10,
+	left = 12,
+	right = 12,
+	top = 10,
+	bottom = 10,
 }
 
 -- 半透明 + macOS 毛玻璃
@@ -65,32 +66,32 @@ config.audible_bell = "Disabled"
 
 -- 热键
 config.keys = {
-  -- 左右分屏
-  {
-    key = "d",
-    mods = "CMD",
-    action = act.SplitHorizontal {
-      domain = "CurrentPaneDomain",
-    },
-  },
+	-- 左右分屏
+	{
+		key = "d",
+		mods = "CMD",
+		action = act.SplitHorizontal({
+			domain = "CurrentPaneDomain",
+		}),
+	},
 
-  -- 上下分屏
-  {
-    key = "d",
-    mods = "CMD|SHIFT",
-    action = act.SplitVertical {
-      domain = "CurrentPaneDomain",
-    },
-  },
+	-- 上下分屏
+	{
+		key = "d",
+		mods = "CMD|SHIFT",
+		action = act.SplitVertical({
+			domain = "CurrentPaneDomain",
+		}),
+	},
 
-  -- 关闭当前 pane
-  {
-    key = "w",
-    mods = "CMD",
-    action = act.CloseCurrentPane {
-      confirm = true,
-    },
-  },
+	-- 关闭当前 pane
+	{
+		key = "w",
+		mods = "CMD",
+		action = act.CloseCurrentPane({
+			confirm = true,
+		}),
+	},
 }
 
 -- 默认文件夹
@@ -100,28 +101,26 @@ local public_ip = "..."
 local last_ip_update = 0
 
 wezterm.on("update-status", function(window, pane)
-  local now = os.time()
+	local now = os.time()
 
-  -- 每 5 分钟更新一次公网 IP
-  if now - last_ip_update > 300 then
-    local success, stdout = wezterm.run_child_process({
-      "curl",
-      "-fsS",
-      "https://api.ipify.org",
-    })
+	-- 每 5 分钟更新一次公网 IP
+	if now - last_ip_update > 300 then
+		local success, stdout = wezterm.run_child_process({
+			"curl",
+			"-fsS",
+			"https://api.ipify.org",
+		})
 
-    if success then
-      public_ip = stdout:gsub("%s+", "")
-    else
-      public_ip = "offline"
-    end
+		if success then
+			public_ip = stdout:gsub("%s+", "")
+		else
+			public_ip = "offline"
+		end
 
-    last_ip_update = now
-  end
+		last_ip_update = now
+	end
 
-  window:set_right_status(
-    string.format("  %s  ", public_ip)
-  )
+	window:set_right_status(string.format("  %s  ", public_ip))
 end)
 
 return config
